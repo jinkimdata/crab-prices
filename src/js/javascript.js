43af1b366a7afcf6d5f93ae4212e3920ea668090
jinkimdata <jinkimdata@gmail.com>
2016-07-28 16:55:12 -0400

    first commit table buildout

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,6 +1,7 @@
-var dataproject = {
+var crabPrices = {
 	init: function(){
-		//dataproject.share();
+		//crabPrices.share();
+		crabPrices.makeTable();
 	},
 	share: function(){
 		$(".icon-twitter").on("click", function(){
@@ -17,9 +18,43 @@ var dataproject = {
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
+	},
+	makeTable: function() {
+		$('#crabTable').DataTable({
+    	    "order": [[0, "desc"]],
+			"paging": false,
+			"bInfo": false,
+			"bFilter": false,
+			"responsive": {
+				details: {
+					renderer: function ( api, rowIdx, columns ) {
+						var data = $.map( columns, function ( col, i ) {
+							return col.hidden ?
+							'<tr class="expandedRow" data-dt-row="'+col.rowIndex+'" data-dt-column="'+col.columnIndex+'">'+
+							'<td>'+col.title+':'+'</td> '+
+							'<td>'+col.data+'</td>'+
+							'</tr>' :
+							'';
+						} ).join('');
+						return data ? $('<table/>').append( data ) : false;
+					}
+				}
+			},
+			"columnDefs": [
+				{className: 'control',
+				orderable: false,
+				targets: 0},
+				{responsivePriority: 1, targets: 1},
+				{responsivePriority: 2, targets: 5},
+				{responsivePriority: 3, targets: 6},
+				{responsivePriority: 4, targets: 2},
+				{responsivePriority: 5, targets: 3},
+				{responsivePriority: 6, targets: 4}
+			]
+		});
 	}
 }
 $(document).ready(function(){
-	dataproject.init();
+	crabPrices.init();
 	console.log("connected");
 });
